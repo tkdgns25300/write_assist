6dbfad47738623209c2af2a5583a7c3e9734fff0
tkdgns25300 <tkdgns25300@gmail.com>
2025-10-07 23:07:25 +0900

    feat: backend initial setup

---
+++ backend/src/main/java/sanghun/project/writeassist/WriteassistApplication.java
@@ -0,0 +1,13 @@
+package sanghun.project.writeassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteassistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriteassistApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/sanghun/project/writeassist/WriteassistApplicationTests.java
@@ -0,0 +1,13 @@
+package sanghun.project.writeassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteassistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
