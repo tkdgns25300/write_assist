2899996904671fb999ebc75f61886e55f2f2aca4
tkdgns25300 <tkdgns25300@gmail.com>
2025-10-15 17:02:41 +0900

    feat: frontend apply rewrites feature

--- frontend/app/next.config.ts
+++ frontend/app/next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://writeassist.hun.it.kr/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
