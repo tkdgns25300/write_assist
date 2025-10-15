7191d70cb19a0f72fdc034e5f73a2a5389413074
tkdgns25300 <tkdgns25300@gmail.com>
2025-10-15 17:28:54 +0900

    refact: frontend delete rewrites feature

--- frontend/app/next.config.ts
+++ frontend/app/next.config.ts
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://writeassist.hun.it.kr/api/:path*',
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
